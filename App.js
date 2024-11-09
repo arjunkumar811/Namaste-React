b9883d1eedf27aa7dcde995fddae8c4526a668d5
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-09 14:50:49 +0530

    app.js

--- App.js
+++ App.js
@@ -1,7 +1,42 @@
-const name1 = React.createElement("h1",
-     { id: "heading" },
-      "hello world");
 
-const root = ReactDOM.createRoot(document.getElementById ("root"));
+{/* <div id="parent">
+    <div id="child">
+        <h1></h1>
+        <h2></h2>
+    </div>
+</div> */}
 
-root.render(name1);
+
+///////////////////////////////////////////////
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+     React.createElement(
+     "div",
+     {id: "child"},
+     React.createElement(
+     "h1",
+     null,
+     "hi there"
+       )
+    )
+);
+    
+
+       const root = ReactDOM.createRoot(document.getElementById ("root"));
+
+       root.render(parent);
+
+
+   
+
+
+
+
+// const name1 = React.createElement("h1",
+//      { id: "heading" },
+//       "hello world");
+
+// const root = ReactDOM.createRoot(document.getElementById ("root"));
+
+// root.render(name1);
