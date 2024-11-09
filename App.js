5f77c62ba81b934eecab6110ea15ba242cd20897
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-09 15:02:12 +0530

    ReactDOM

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 
 {/* <div id="parent">
+    
     <div id="child">
         <h1> hi there h1 </h1>
         <h2> hi there h2 </h2>
     </div>
+
+    <div id="child">
+        <h1> hi there h1 </h1>
+        <h2> hi there h2 </h2>
+    </div>
+
 </div> */}
 
 
@@ -11,17 +18,28 @@
 const parent = React.createElement(
     "div",
     { id: "parent" },
-     React.createElement(
-     "div",
-     {id: "child"},
-     [React.createElement("h1", null, "hi there"), React.createElement("h1", null, "hi there")] 
+    React.createElement(
+        "div",
+        { id: "child" },
+        [
+            React.createElement("h1", null, "hi there"),
+            React.createElement("h2", null, "hi there")
+        ],
+        React.createElement(
+            "div",
+            { id: "child2" },
+            [
+                React.createElement("h1", null, "h1 there"),
+                React.createElement("h2", null, "hi there")
+            ]
+        )
     )
 );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
     
-
-       const root = ReactDOM.createRoot(document.getElementById ("root"));
-
-       root.render(parent);
 
 
    
