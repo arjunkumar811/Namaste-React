0ccd0bda867006446aa656d48124d44631f4f74d
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-10 19:27:04 +0530

    creating heading element using React.createElement

--- Episode 03 - Laying the Foundation/Code/App.js
+++ Episode 03 - Laying the Foundation/Code/App.js
@@ -1,2 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// creating heading element using React.createElement
+
+// React.createElement => ReactElement (JS Object) => HTMLElement (render) => DOM (Browser)
+
+
+const heading = React.createElement("h1", { id: "heading" }, "Namasate React 🚀");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
