e5925cb5771676208594ac242c8396633f20c2c2
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-10 19:51:27 +0530

    React component

--- Episode 03 - Laying the Foundation/Code/App.js
+++ Episode 03 - Laying the Foundation/Code/App.js
@@ -13,7 +13,45 @@ import ReactDOM from "react-dom/client";
 
 // Create heading element using JSX
 
-const jsxHeading = <h1 id="heading" > Namasate react asfd  </h1>
+// const jsxHeading = <h1 id="heading" > Namasate react asfd  </h1>
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading);
+
+
+/**
+ * React Component
+ * Functional component - new way of writeing component
+ * Calss component - old way of writeing compoent
+ * React component is a function or class that return a React element (JS object)
+ */
+
+// React Component
+const elem = "Arjun"
+// const Title = () => {
+// return <h1 className="head" id="heading">
+//     <h1>{elem}</h1> Namasate React
+// </h1>
+// }
+
+const Title = () => (
+<h1 className="head" id="heading">
+    <h1 style={{color: "red"}}>{elem}</h1>
+    Namasate React
+</h1>
+);
+
+// React Fragment: Behaves like an empty tag
+// Components Composition - multiple components can be composed together to create a new component
+
+const HeadingComponent = () => (
+    <>
+    <Title/>
+     {/* {Title()} */}
+      {/* <Title></Title> */}
+    </>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+
+root.render(<HeadingComponent/> );
