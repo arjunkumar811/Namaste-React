540ec76345bedc67d49e013dadf2523a2162512c
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-11 13:22:01 +0530

    Added Assignment Solution

---
+++ Episode 03 - Laying the Foundation/Practice-react/Assignment Solution/Code Assignment/Coding - 1/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//Q1 - Create a Nested header Element using React.createElement(h1,h2,h3 inside a
+// div with class “title”)
+
+ /** const header = React.createElement("div", {className: "title"} ,  [
+    React.createElement("h1", null, "hi there 1"),
+    React.createElement("h2", null, "hi there 2"),
+    React.createElement("h3", null, "hi there 3")
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(header); */
+
